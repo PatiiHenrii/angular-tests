ca5049da2d506a736b4a8d3d3f2ca688cf36a16c
patricia.damasceno <patricia.damasceno>
2021-10-15 17:07:35 -0300

    first commit

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,54 @@
+import { TestBed } from '@angular/core/testing';
+
+import { UniqueIdService } from './unique-id.service';
+
+describe(UniqueIdService.name, () => {
+  let service: UniqueIdService;
+  beforeEach(() => {
+    // TestBed.configureTestingModule({});
+    service = new UniqueIdService();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id.startsWith('app-')).toBeTruthy();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate dublicate ids`, () => {
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
+
+  it(`#${UniqueIdService.prototype.getnumberOfGeneratedIds.name} should return the number of generated ids when caled`, () => {
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
+    const numberOfGeneratedIds = service.getnumberOfGeneratedIds();
+    expect(numberOfGeneratedIds).toBe(2);
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should throw when called with empty`, () => {
+    expect(service.generateUniqueIdWithPrefix('')).toThrow();
+  });
+});
+
+// describe('UniqueIdService', () => {
+//   let service: UniqueIdService;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(UniqueIdService);
+//   });
+
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+
+//   it('#generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+//     const service = new UniqueIdService();
+//     const id = service.generateUniqueIdWithPrefix('app');
+//     expect(id).toContain('app-');
+//   });
+// });
